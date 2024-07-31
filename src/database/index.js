0a1a154fc598f24406783e600e32119f233d7a65
xezernn <xezernovruz79@gmail.com>
2024-07-31 18:31:55 +0400

    update

--- src/database/index.js
+++ src/database/index.js
@@ -1,12 +1,14 @@
 const path = require("path");
 const { Sequelize } = require("sequelize");
 const config = require("../config");
+const pg = require('pg')
 
 const dbPath = path.join(__dirname, "../../db.sqlite");
 
 const sequelize = new Sequelize(config.POSTGRES_URL, {
   // storage: dbPath,
   dialect: "postgres",
+  dialectModule: pg,
   sync: true,
   logging: console.log,
 });
