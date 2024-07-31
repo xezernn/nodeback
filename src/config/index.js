9292397481f51f011141512f227edd1197daa704
xezernn <xezernovruz79@gmail.com>
2024-07-31 18:29:41 +0400

    added postgres

--- src/config/index.js
+++ src/config/index.js
@@ -8,4 +8,5 @@ dotenv.config({ path: envPath });
 module.exports = {
   port: process.env.PORT,
   jwtSecret: process.env.JWT_SECRET,
+  POSTGRES_URL: process.env.POSTGRES_URL
 };
--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,12 @@
 const path = require("path");
 const { Sequelize } = require("sequelize");
+const config = require("../config");
 
 const dbPath = path.join(__dirname, "../../db.sqlite");
 
-const sequelize = new Sequelize({
-  storage: dbPath,
-  dialect: "sqlite",
+const sequelize = new Sequelize(config.POSTGRES_URL, {
+  // storage: dbPath,
+  dialect: "postgres",
   sync: true,
   logging: console.log,
 });
